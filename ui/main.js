0a68951aea5d4bae8f12942560dc05ee77b3bc0f
avinash bhoyar <avi2012bhoyar@gmail.com>
2017-08-05 17:33:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,18 @@ var element= document.getElementById('main-text');
 element.innerHTML= 'New Value';
 
 // move the image
-var image= document.getElementById('modi');
+var img= document.getElementById('modi');
 
-image.onclick = function() {
-   image.style.marginLeft = "200px";
+/*img.onclick = function() {
+   img.style.marginLeft = "200px";
+}; */
+
+var marginLeft=0;
+function moveRight(){
+    marginLeft= marginLeft+1;
+    img.style.marginLeft= marginLeft+ 'px';
+}
+
+img.onclick=function(){
+    var interval= setInterval(moveRight,50);
 }; 
-
