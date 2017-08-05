787fd2574ed567a64b90da91ac565f665af8861f
avinash bhoyar <avi2012bhoyar@gmail.com>
2017-08-05 23:30:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,6 @@ button.onclick = function() {
 };
 
 // Submit name
-var nameInput= document.getElementById('name');
-var name= nameInput.value;
 var submit= document.getElementById('submit_btn');
 submit.onclick= function() {
     
@@ -54,6 +52,8 @@ submit.onclick= function() {
     };
     
     // make request
+    var nameInput= document.getElementById('name');
+    var name= nameInput.value;
     request.open("GET","http://avi2012bhoyar.imad.hasura-app.io/submit-name?name="+name,true);
     request.send(null);
     
