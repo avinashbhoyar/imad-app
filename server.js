724a7fc86e70b7094eb4b4a81dfa1540299fad18
avinash bhoyar <avi2012bhoyar@gmail.com>
2017-08-15 20:11:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,7 +138,14 @@ var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
   // make a select request 
   // return a response with the results
-  
+  pool.query('SELECT * FROM test', function(err,result){
+     if(err){
+         res.status(500).send(err.tostring());
+     } else{
+         res.send(JSON.stringify(result));
+     }
+     
+  });
 });
 
 
