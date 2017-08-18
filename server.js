0433b4b29adce87e1950e3eb458abff0c606294a
avinash bhoyar <avi2012bhoyar@gmail.com>
2017-08-18 19:21:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.get('/articles/:articleName',function(req,res){
            if(result.rows.length===0){
                res.status(404).send('article not found');
            }else{
-               var articleData= result.rows(0);
+               var articleData= result.rows[0];
                res.send(createTemplate(articleData));
            }
        }
