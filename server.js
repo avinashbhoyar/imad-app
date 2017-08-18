2f65af623c5bc0362d3bff454a078bbaa9b7249f
avinash bhoyar <avi2012bhoyar@gmail.com>
2017-08-18 19:14:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.get('/submit-name', function (req, res) {
 });
 
 
-app.get('/article/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
     // articleName== article-one
     // articles[articleName]== {} content object for article one
     
