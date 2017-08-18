ec803b6f26ff8c21ebc37268f07bb5737eba4ade
avinash bhoyar <avi2012bhoyar@gmail.com>
2017-08-18 19:29:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,6 +51,25 @@ var articles={
 
 };
 
+app.get('/articles/:articleName',function(req,res){
+    // articleName== article-one
+    // articles[articleName]== {} content object for article one
+    
+    // SELECT * FROM article WHERE title='article-one'
+    pool.query("SELECT * FROM article WHERE title='"+ req.params.articleName+"'",function(err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           if(result.rows.length===0){
+               res.status(404).send('article not found');
+           }else{
+               var articleData= result.rows[0];
+               res.send(createTemplate(articleData));
+           }
+       }
+    });
+});
+
 function createTempate(data){
     var title=data.title;
     var heading=data.heading;
@@ -135,24 +154,7 @@ app.get('/submit-name', function (req, res) {
 });
 
 
-app.get('/articles/:articleName',function(req,res){
-    // articleName== article-one
-    // articles[articleName]== {} content object for article one
-    
-    // SELECT * FROM article WHERE title='article-one'
-    pool.query("SELECT * FROM article WHERE title='"+ req.params.articleName+"'",function(err, result){
-       if(err){
-           res.status(500).send(err.toString());
-       }else{
-           if(result.rows.length===0){
-               res.status(404).send('article not found');
-           }else{
-               var articleData= result.rows[0];
-               res.send(createTemplate(articleData));
-           }
-       }
-    });
-});
+
 
 
 // handling specific url
